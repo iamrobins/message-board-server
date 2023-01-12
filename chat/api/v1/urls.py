e28b700cfd323fb1bc120c3f772910ca4043f750
iamrobins <itsrobin4@gmail.com>
2023-01-12 15:39:15 +0530

    Features Added/Improved
    - Toxicity Check Added Messages
    - Get Secret Token and Delete Action Added

--- chat/api/v1/urls.py
+++ chat/api/v1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MessageAPIView
+from .views import MessageAPIView, AdminAcessAPIView
 
 
 urlpatterns = [
   path("message/", MessageAPIView.as_view(), name="message"),
+  path("admin-access/", AdminAcessAPIView.as_view(), name="admin-access"),
 ]
--- chat/api/v1/views.py
+++ chat/api/v1/views.py
@@ -1,28 +1,65 @@
+import traceback
+import logging
 from rest_framework import exceptions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from chat.models import Message
 from chat.serializers import MessageSerializer
 from chat import utils
-
+from chat.permissions import JWTAuthentication
 
 class MessageAPIView(APIView):
-
   def get(self, request):
-    messages = Message.objects.all()
-    serializer = MessageSerializer(messages, many=True)
+    try:
+      messages = Message.objects.all().order_by('-created_at')[:10]
+      # messages = Message.objects.all()
+      serializer = MessageSerializer(messages, many=True)
+    except Exception as e:
+      logging.error(traceback.format_exc())
+      return Response(str(e), 400)
 
     return Response(serializer.data, 200)
 
   def post(self, request):
-    ip_addr = utils.get_ip_address(request)
-    if not ip_addr:
-      return Response("Unable to get the ip_address", 400)
+    try:
+      ip_addr = utils.get_ip_address(request)
+      if not ip_addr: return Response("Unable to get the ip_address", 400)
 
-    request.data["source"] = ip_addr
-    
-    serializer = MessageSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
+      is_toxic = utils.check_toxicity(request.data["content"])
+      if is_toxic: return Response("Toxicity Not Allowed", 400)
+
+      request.data["source"] = ip_addr
+      serializer = MessageSerializer(data=request.data)
+      serializer.is_valid(raise_exception=True)
+      serializer.save()
+    except Exception as e:
+      logging.error(traceback.format_exc())
+      return Response(str(e), 400)
     
     return Response(serializer.data, 201)
+
+  def delete(self, request):
+    try:
+      payload = JWTAuthentication.authenticate(request)
+      if payload["scope"] != "admin": raise Exception("Only admins are allowed to perform this action")
+      
+      id = request.query_params.get("id")
+      message = Message.objects.filter(id=id)
+      if not message:
+        raise Exception("Message not found")
+      message.delete()
+      
+    except Exception as e:
+      logging.error(traceback.format_exc())
+      return Response(str(e), 400)
+    return Response("Message successfully deleted", 204)
+
+class AdminAcessAPIView(APIView):
+  def get(self, request):
+    token = JWTAuthentication.generate_jwt(scope="admin")
+    response = Response()
+
+    response.set_cookie("secret", token, httponly=True)
+    response.data = {"token": token}
+
+    return response
