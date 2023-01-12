70f90f911c9fa6bc23a2def6c11a6cadcb523951
iamrobins <itsrobin4@gmail.com>
2023-01-12 15:36:59 +0530

    toxicity check model added

--- chat/utils.py
+++ chat/utils.py
@@ -1,12 +1,15 @@
-def get_ip_address(request):
+from detoxify import Detoxify
+
+def get_ip_address(request) -> str:
     req_headers = request.META
-    try:
-        x_forwarded_for_value = req_headers.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_for_value:
-            ip_addr = x_forwarded_for_value.split(',')[-1].strip()
-        else:
-            ip_addr = req_headers.get('REMOTE_ADDR')
-    except e:
-        print("Error getting the ip address from request")
-        ip_addr = None
+    x_forwarded_for_value = req_headers.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for_value:
+        ip_addr = x_forwarded_for_value.split(',')[-1].strip()
+    else:
+        ip_addr = req_headers.get('REMOTE_ADDR')
     return ip_addr
+
+
+predictor = Detoxify('original')
+def check_toxicity(message: str) -> bool:
+    return True if predictor.predict(message)["toxicity"] > 0.89 else False
