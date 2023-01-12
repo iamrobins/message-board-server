05cab8afd91152fb2759694e667a8e8113a6d026
iamrobins <itsrobin4@gmail.com>
2023-01-12 15:37:18 +0530

    jwt auth added

---
+++ chat/permissions.py
@@ -0,0 +1,32 @@
+import jwt
+import datetime
+from rest_framework import exceptions
+from rest_framework.authentication import BaseAuthentication
+from message_board.settings import SECRET_KEY
+
+
+class JWTAuthentication(BaseAuthentication):
+
+  @staticmethod
+  def authenticate(request):
+      token = request.COOKIES.get("secret")
+
+      if not token:
+        return None
+
+      try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+      except jwt.ExpiredSignatureError:
+        raise exceptions.AuthenticationFailed("unauthenticated")
+
+      return payload
+
+  @staticmethod
+  def generate_jwt(scope: str):
+    payload = {
+      "scope": scope,
+      "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=10),
+      "iat": datetime.datetime.utcnow()
+    }
+
+    return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
