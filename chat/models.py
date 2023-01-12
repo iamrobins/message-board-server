4b5b833ce8bb6df1800ec92462469b1ad1573cb5
iamrobins <itsrobin4@gmail.com>
2023-01-12 22:50:43 +0530

    Updated Message Model with Name

--- chat/models.py
+++ chat/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Message(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, blank=False)
     content = models.TextField(blank=False)
     source = models.CharField(max_length=255, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
